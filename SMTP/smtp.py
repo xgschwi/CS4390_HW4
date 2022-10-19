1af7449514371a608f817d2586b3f308f2eddbc3
Xavier Gschwind <xgschwi@bgsu.edu>
2022-10-19 17:10:39 -0400

    Documentation Added

--- SMTP/smtp.py
+++ SMTP/smtp.py
@@ -2,8 +2,13 @@ from smtpClass import smtpClient
 import sys
 args = sys.argv[1:]
 
-client = smtpClient(args[0],args[1])#'smtp.bgsu.edu', 25)
-client.senderAndReceiver(args[2], args[3])#'xgschwi@bgsu.edu', 'xgschwi@bgsu.edu')
-client.messageBody(args[4], args[5])#'Hi from yourself. HW4')
-client.sendEmail()
-client.endTheSession()
+client = smtpClient(args[0],args[1]) #SMTP Server Name should be smtp.bgsu.edu with PortNo 25
+client.senderAndReceiver(args[2], args[3]) # Sender and Receiver of emails
+(status, err) = client.messageBody(args[4], args[5]) #Pass message content and attachment file location relative to call on functions
+
+if status == -1: #Error occurred with Attachment
+    print('Error in Message Body: ', err)
+    client.endTheSession()
+else: # Message and Attachment Ready
+    client.sendEmail()
+    client.endTheSession()
--- SMTP/smtpClass.py
+++ SMTP/smtpClass.py
@@ -1,35 +1,39 @@
 from smtplib import SMTP # https://docs.python.org/3/library/smtplib.html
-    # attachments next https://docs.python.org/3/library/email.compat32-message.html#email.message.Message.add_header
+    # MIME Type message with attachment https://docs.python.org/3/library/email.compat32-message.html#email.message.Message.add_header
     # https://docs.python.org/3/library/email.mime.html
-
+    # Header for file attachment https://blog.mailfence.com/email-header/
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
 
-class smtpClient:	        #the return value & parameters are just informational - you decide what they need to be.
+class smtpClient:
     message = ''
     server = None
     sender = ''
     receiver = ''
     
+    # Set the sender and receiver of emails
     def senderAndReceiver(self, sender, receiver):
         self.sender = sender
         self.receiver = receiver
 
+    # Set the message body, which is a MIME type of multiple parts, with message body and attachment
     def messageBody(self, message, attachment=None):
         messageMulti = MIMEMultipart()
         messageMulti.attach(MIMEText(message))
 
-        attachmentPart = None
-        with open(attachment, 'rb') as a:
-            attachmentPart = MIMEApplication(a.read())
-        attachmentPart.add_header('Content-Disposition', 'attachment;filename=' + attachment)
+        try:
+            attachmentPart = None
+            with open(attachment, 'rb') as a: # Read bytes of attachment
+                attachmentPart = MIMEApplication(a.read()) # MIMEApplication takes raw bytes of data for attachment and defaults to recognize an octet stream
+            attachmentPart.add_header('Content-Disposition', 'attachment;filename=' + attachment) # Header for File Attachment
 
-        messageMulti.attach(attachmentPart)
-
-        self.message = messageMulti.as_string()
-
-
+            messageMulti.attach(attachmentPart)
+            self.message = messageMulti.as_string()
+        except Exception as e:
+            return (-1, e)
+        
+        return (1, None)
 
     def sendEmail(self):
         self.server.sendmail(from_addr=self.sender, to_addrs=[self.receiver], msg=self.message)
