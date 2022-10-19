56c907527dba6997f60b158f49726d6a71be751a
Xavier Gschwind <xgschwi@bgsu.edu>
2022-10-19 16:53:33 -0400

    Adding command line with Attachment

--- SMTP/smtp.py
+++ SMTP/smtp.py
@@ -1,7 +1,9 @@
 from smtpClass import smtpClient
+import sys
+args = sys.argv[1:]
 
-client = smtpClient('smtp.bgsu.edu', 25)
-client.senderAndReceiver('xgschwi@bgsu.edu', 'xgschwi@bgsu.edu')
-client.messageBody('Hi from yourself. HW4')
+client = smtpClient(args[0],args[1])#'smtp.bgsu.edu', 25)
+client.senderAndReceiver(args[2], args[3])#'xgschwi@bgsu.edu', 'xgschwi@bgsu.edu')
+client.messageBody(args[4], args[5])#'Hi from yourself. HW4')
 client.sendEmail()
 client.endTheSession()
--- SMTP/smtpClass.py
+++ SMTP/smtpClass.py
@@ -1,5 +1,12 @@
+from msilib.schema import MIME
 from smtplib import SMTP # https://docs.python.org/3/library/smtplib.html
     # attachments next https://docs.python.org/3/library/email.compat32-message.html#email.message.Message.add_header
+    # https://docs.python.org/3/library/email.mime.html
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from email.mime.text import MIMEText
+
 class smtpClient:	        #the return value & parameters are just informational - you decide what they need to be.
     message = ''
     server = None
@@ -10,8 +17,20 @@ class smtpClient:	        #the return value & parameters are just informational 
         self.sender = sender
         self.receiver = receiver
 
-    def messageBody(self, message):
-        self.message = message
+    def messageBody(self, message, attachment=None):
+        messageMulti = MIMEMultipart()
+        messageMulti.attach(MIMEText(message))
+
+        attachmentPart = None
+        with open(attachment, 'rb') as a:
+            attachmentPart = MIMEApplication(attachment.read())
+        attachmentPart.add_header('Content-Disposition', 'attachment;filename=' + attachment)
+
+        messageMulti.attach(attachmentPart)
+
+        self.message = messageMulti.as_string()
+
+
 
     def sendEmail(self):
         self.server.sendmail(from_addr=self.sender, to_addrs=[self.receiver], msg=self.message)
